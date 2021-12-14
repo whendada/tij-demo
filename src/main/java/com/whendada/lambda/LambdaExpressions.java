f729a2351eadd2e9f4dd0b0d3679e2a7a6820c5f
wenkj <wenkj@ziroom.com>
2021-12-14 09:33:28 +0800

    lambda

---
+++ src/main/java/com/whendada/lambda/LambdaExpressions.java
@@ -0,0 +1,39 @@
+package com.whendada.lambda;
+
+interface Description {
+    String brief();
+}
+
+interface Body {
+    String detailed(String head);
+}
+
+interface Multi {
+    String twoArg(String head, Double d);
+}
+
+public class LambdaExpressions {
+
+    // lambda最后返回的就是方法执行之后的结果
+
+    static Body bod = h -> h + " No Parens!";      // [1]
+
+    static Body bod2 = (h) -> h + " More details"; // [2]
+
+    static Description desc = () -> "Short info";  // [3]
+
+    static Multi mult = (h, n) -> h + n;           // [4]
+
+    static Description moreLines = () -> {         // [5]
+        System.out.println("moreLines()");
+        return "from moreLines()";
+    };
+
+    public static void main(String[] args) {
+        System.out.println(bod.detailed("Oh!"));
+        System.out.println(bod2.detailed("Hi!"));
+        System.out.println(desc.brief());
+        System.out.println(mult.twoArg("Pi! ", 3.14159));
+        System.out.println(moreLines.brief());
+    }
+}
